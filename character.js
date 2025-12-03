ae4075d6eda93c835bf356e96178b7f12828658a
Carwyn-G <gica25fx@student.ju.se>
2025-12-03 11:14:59 +0100

    Add: score

--- character.js
+++ character.js
@@ -1,47 +1,58 @@
 export default class Character {
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
 
-    	constructor(x, y, w, h) {
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.h = h;
+    this.velocity = 0;
+    this.gravity = 0.9;
+    this.jumpPower = 9;
+  }
 
-        this.velocity = 0;
-        this.gravity = .98;
-        this.jumpPower = 20;
-	}
+  draw() {
+    rect(this.x, this.y, this.w, this.h);
+  }
 
-    draw() {
-        rect(this.x, this.y, this.w, this.h);
+  update() {
+    if (this.x + this.w < 0) this.x = canvasWidth;
+    if (this.x > canvasWidth) this.x = -this.w;
 
+    this.velocity += this.gravity;
+    this.y += this.velocity;
+
+    if (keyIsDown(39) === true) {
+      {
+        this.x += 4;
+      }
+    }
+    if (keyIsDown(37) === true) {
+      {
+        this.x -= 4;
+      }
     }
 
-    update(){
-        if (this.x + this.w < 0) this.x = canvasWidth;
-        if (this.x > canvasWidth) this.x = -this.w;
+    for (let platform of platforms) {
+      if (
+        this.y + this.h >= platform.y &&
+        this.y + this.h <= platform.y + platform.h
+      ) {
+        let minX = platform.x - this.w;
+        let maxX = platform.x + platform.w;
 
-        this.velocity += this.gravity;
-        this.y += this.velocity;
-
-        if (keyIsDown(39)===true){
-        {this.x += 15;}
-        }
-        if (keyIsDown(37)===true){
-        {this.x -= 15;}
-        }
-    }
-    
-    jump(){
-        this.velocity -= this.jumpPower;
+        if (this.x >= minX && this.x <= maxX) this.jump();
+      }
     }
 
-    isColliding(character, platform) {
-    if (platform.y === character.y + character.h && 
-        platform.x <= character.x + character.w &&
-        platform.x + platform.w >= character.x){
-        return true;
-    } else {
-        return false;
+    if (this.velocity < -9) this.velocity = -9;
+
+    if (platforms[0].y > character.y + 400) {
+      platforms.splice(0, 1);
+      score++;
     }
-}
+  }
+
+  jump() {
+    this.velocity -= this.jumpPower;
+  }
 }
--- game.js
+++ game.js
@@ -6,39 +6,50 @@ let canvasHeight = 600;
 let character;
 let gap;
 let platforms = [];
+let score = 0;
 
 function setup() {
-    createCanvas(canvasWidth, canvasHeight);
-    colorMode(HSB);
-    character = new Character(canvasWidth/2,canvasHeight/2,50,80);
+  createCanvas(canvasWidth, canvasHeight);
+  colorMode(HSB);
+  character = new Character(canvasWidth / 2, canvasHeight / 2, 50, 80);
 
-    let platformCount = 6;
-    gap = canvasHeight/platformCount;
-    for (let i = 1; i < platformCount; i++) {
-        platforms.push(new Platform(random(canvasWidth), canvasHeight - i*gap));
-    }
+  let platformCount = 6;
+  gap = canvasHeight / platformCount;
+  for (let i = 1; i < platformCount; i++) {
+    platforms.push(new Platform(random(canvasWidth), canvasHeight - i * gap));
+  }
 }
 
 function draw() {
-    background(256,32,100);
+  background(256, 32, 100);
 
-    translate(0, canvasWidth / 2 - character.y);
+  translate(0, canvasWidth / 2 - character.y);
 
-    character.draw();
-    character.update();
-    
-    for (let platform of platforms) {
-        platform.draw();
-    }
+  character.draw();
+  character.update(platforms);
 
-    if (character.y < platforms[platforms.length-1].y + 200) {
-        platforms.push(new Platform(random(canvasWidth), platforms[platforms.length-1].y - gap));
-    }
+  for (let platform of platforms) {
+    platform.draw();
+  }
 
+  if (character.y < platforms[platforms.length - 1].y + 200) {
+    platforms.push(
+      new Platform(random(canvasWidth), platforms[platforms.length - 1].y - gap)
+    );
+  }
+
+  push();
+  fill(0, 0, 100);
+  textSize(30);
+  textAlign(CENTER);
+  text(score, canvasWidth / 2, character.y - 150);
+  pop();
 }
 
 function keyPressed() {
-    if (keyCode === 38) {
-        {character.jump();}
+  if (keyCode === 38) {
+    {
+      character.jump();
     }
-    }
+  }
+}
