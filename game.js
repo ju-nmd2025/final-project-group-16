61d4d06d6af78e3efb227faf9647a9adb0c94412
linus <linus.sonnerhed@ju.se>
2025-12-11 11:01:39 +0100

    Fix: score now character property, added update parameters and arguments

--- game.js
+++ game.js
@@ -6,7 +6,6 @@ let canvasHeight = 600;
 let character;
 let gap;
 let platforms = [];
-let score = 0;
 let difficulty = 10; //sets score at which new platform variants start to appear
 let speed = 6;
 let gameMode = "start";
@@ -17,7 +16,6 @@ function setup() {
   colorMode(HSB);
 
   platforms = [];
-  score = 0;
 
   character = new Character(canvasWidth / 2 - 25, canvasHeight - gap, 50, 60);
 
@@ -67,22 +65,22 @@ function gameScreen() {
   translate(0, canvasHeight / 2 - character.y);
 
   //high score tracker
-  if (score >= highScore) {
-    highScore = score;
+  if (character.score >= highScore) {
+    highScore = character.score;
   }
 
   push();
   fill(0, 0, 0);
   textSize(30);
   textAlign(CENTER);
-  text(score, canvasWidth / 2, character.y - 200);
+  text(character.score, canvasWidth / 2, character.y - 200);
   textSize(20);
   textAlign(CENTER);
   text(`High Score:  ${highScore}`, canvasWidth / 2, character.y + 250);
   pop();
 
   character.draw();
-  character.update(platforms);
+  character.update(platforms, canvasWidth);
 
   for (let platform of platforms) {
     platform.draw();
@@ -98,49 +96,49 @@ function gameScreen() {
     );
   }
   //make the platforms move after a certain point, but freeze once jumped on
-  if (score > difficulty) {
-    platforms[4].x = platforms[4].x + speed;
-    if (platforms[4].x > canvasWidth) {
-      platforms[4].x = 0 - platforms[4].w;
-    }
+  if (character.score > difficulty) {
+      platforms[4].x = platforms[4].x + speed;
+      if (platforms[4].x > canvasWidth) {
+          platforms[4].x = 0 - platforms[4].w;
+      }
   }
 
-  if (score > difficulty + 1) {
-    platforms[3].x = platforms[3].x + speed;
-    if (platforms[3].x > canvasWidth) {
-      platforms[3].x = 0 - platforms[3].w;
-    }
+  if (character.score > difficulty + 1) {
+      platforms[3].x = platforms[3].x + speed;
+      if (platforms[3].x > canvasWidth) {
+          platforms[3].x = 0 - platforms[3].w;
+      }
   }
 
-  if (score > difficulty + 2) {
-    platforms[2].x = platforms[2].x + speed;
-    if (platforms[2].x > canvasWidth) {
-      platforms[2].x = 0 - platforms[2].w;
-    }
+  if (character.score > difficulty + 2) {
+      platforms[2].x = platforms[2].x + speed;
+      if (platforms[2].x > canvasWidth) {
+          platforms[2].x = 0 - platforms[2].w;
+      }
   }
 
-  if (score > difficulty * 2) {
-    //at an even further point the platforms no longer freeze after being jumped on
-    platforms[1].x = platforms[1].x + speed;
-    if (platforms[1].x > canvasWidth) {
-      platforms[1].x = 0 - platforms[1].w + speed;
-    }
+  if (character.score > difficulty * 2) {
+      //at an even further point the platforms no longer freeze after being jumped on
+      platforms[1].x = platforms[1].x + speed;
+      if (platforms[1].x > canvasWidth) {
+          platforms[1].x = 0 - platforms[1].w + speed;
+      }
   }
 
-  if (score > difficulty * 2 + 1) {
-    platforms[0].x = platforms[0].x + speed;
-    if (platforms[0].x > canvasWidth) {
-      platforms[0].x = 0 - platforms[0].w + speed;
-    }
+  if (character.score > difficulty * 2 + 1) {
+      platforms[0].x = platforms[0].x + speed;
+      if (platforms[0].x > canvasWidth) {
+          platforms[0].x = 0 - platforms[0].w + speed;
+      }
   }
 
   //at an even further point the platforms disappear after being jumped on
-  if (score > difficulty * 3) {
-    platforms[0].x = 1000 + speed * 0;
+  if (character.score > difficulty * 3) {
+      platforms[0].x = 1000 + speed * 0;
   }
 
-  if (score > difficulty * 3 + 1) {
-    platforms[1].x = 1000 + speed * 0;
+  if (character.score > difficulty * 3 + 1) {
+      platforms[1].x = 1000 + speed * 0;
   }
 }
 
@@ -150,7 +148,7 @@ function gameOver() {
   fill(0, 0, 0);
   text("You scored:", canvasWidth / 2, canvasHeight / 8 - 20);
   textSize(30);
-  text(score, canvasWidth / 2, canvasHeight / 8 + 25);
+  text(character.score, canvasWidth / 2, canvasHeight / 8 + 25);
   textSize(25);
   text("Press SPACE to play again", canvasWidth / 2, canvasHeight / 8 + 70);
   gameMode = "end";
